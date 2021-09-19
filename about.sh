8c98a67488c8c470b3ddfdf06b608992cea64f1a
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2021-09-20 05:08:58 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -5,12 +5,15 @@ BLUE='\e[0;34m'
 NC='\e[0m'
 
 clear
-echo -e "$=================================================" | lolcat
+echo -e "=================================================" | lolcat
 echo -e "         ${RED} Premium Auto Script By THIRASTORE${NC}"
-echo -e "$=================================================" | lolcat
+echo -e "=================================================" | lolcat
+echo -e ""
 echo -e " System Operation Support:                     "
 echo -e " - Debian 9 & Debian 10 64 Bit                 "
 echo -e " - Ubuntu 18.04 & Ubuntu 20.04 64 Bit          "
 echo -e " For VPS with KVM and VMWare virtualization    "
+echo -e " Whatsapp : wa.me/+6285717614888"
+echo -e " Telegram : @T_STORE17"
 echo -e ""
-echo -e "$=================================================" | lolcat
+echo -e "=================================================" | lolcat
