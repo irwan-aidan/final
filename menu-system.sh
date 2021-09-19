f0c514052e4bc8b644be2debe45c571dda38311c
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2021-09-20 05:28:10 +0700

    Update menu-system.sh

--- menu-system.sh
+++ menu-system.sh
@@ -3,21 +3,21 @@ clear
 echo -e "==========================[ Menu VPS ]========================" | lolcat
 echo -e "==============================================================" | lolcat
 echo -e ""
-echo -e "	*	[1]  Set Auto Reboot VPS" | lolcat
-echo -e "	*	[2]  Change Port Of Some Service" | lolcat
-echo -e "	*	[3]  Autobackup Data VPS" | lolcat
-echo -e "	*	[4]  Backup Data VPS" | lolcat
-echo -e "	*	[5]  Restore Data VPS" | lolcat
-echo -e "	*	[6]  Webmin Menu" | lolcat
-echo -e "	*	[7]  Update To Latest Kernel" | lolcat
-echo -e "	*	[8]  Check Usage Bandwith Server" | lolcat
-echo -e "	*	[9]  Limit Bandwith Speed Server" | lolcat
-echo -e "	*	[10] Check Usage of VPS Ram" | lolcat
-echo -e "	*	[11] Reboot VPS" | lolcat
-echo -e "	*	[12] Speedtest VPS" | lolcat
-echo -e "	*	[13] Update To Latest Script Version" | lolcat
-echo -e "	*	[14] Displaying System Information" | lolcat
-echo -e "	*	[15] Info Script Auto Install" | lolcat
+echo -e "	*	[1]  Set Auto Reboot VPS" 
+echo -e "	*	[2]  Change Port Of Some Service"
+echo -e "	*	[3]  Autobackup Data VPS" 
+echo -e "	*	[4]  Backup Data VPS" 
+echo -e "	*	[5]  Restore Data VPS"
+echo -e "	*	[6]  Webmin Menu" 
+echo -e "	*	[7]  Update To Latest Kernel" 
+echo -e "	*	[8]  Check Usage Bandwith Server"
+echo -e "	*	[9]  Limit Bandwith Speed Server" 
+echo -e "	*	[10] Check Usage of VPS Ram" 
+echo -e "	*	[11] Reboot VPS" 
+echo -e "	*	[12] Speedtest VPS" 
+echo -e "	*	[13] Update To Latest Script Version" 
+echo -e "	*	[14] Displaying System Information" 
+echo -e "	*	[15] Info Script Auto Install"
 echo -e ""
 echo -e "==============================================================" | lolcat
 read -p "     Select From Options [number or x] :  " num
