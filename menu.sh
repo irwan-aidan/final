74116e21673e3a3c97f4d5d8804357512203aaa4
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2021-09-19 12:46:40 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -4,7 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- https://icanhazip.com);
 izin
-clear
 COUNTRY=$(curl -s ipinfo.io/country )
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
