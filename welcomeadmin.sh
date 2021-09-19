356a8c932c981f6b654f63248b10cd9a57ee030f
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2021-09-19 18:59:42 +0700

    Update welcomeadmin.sh

--- welcomeadmin.sh
+++ welcomeadmin.sh
@@ -5,5 +5,5 @@ NUMBER=`echo "\033[33m"` #yellow
 LRED=`echo "\033[0m\033[1;31m"`
 neofetch | lolcat
 echo -e ""
-echo -e "${NUMBER}-Silahkan ketik ${LRED}menu${NUMBER} untuk masuk ke Fitur Menu VPS ${NORMAL}"
+echo -e "${NUMBER} #Silahkan ketik ${LRED}menu${NUMBER} untuk masuk ke Fitur Menu VPS ${NORMAL}"
 echo -e ""
