6d3412fbeecd205d04d0469baac7be1891b7172b
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2021-09-20 03:43:59 +0700

    Update add-l2tp.sh

--- add-l2tp.sh
+++ add-l2tp.sh
@@ -3,7 +3,22 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- https://icanhazip.com);
-izin
+IZIN=$(curl https://raw.githubusercontent.com/Dork96/rentScript/main/ipvps | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e ""
+echo -e "${green}Permission Accepted...${NC}"
+else
+clear
+echo -e ""
+echo -e "======================================="
+echo -e "${red}===[ Permission Denied...!!! ]===${NC}";
+echo -e "Contact WA https//wa.me/+6285717614888"
+echo -e "For Registration IP VPS"
+echo -e "======================================="
+echo -e ""
+exit 0
+fi
 clear
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
